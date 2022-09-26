e17282da22ffc00889f7e3efde8b33c178a7e78d
Hein Andre Grønnestad <hag@haugnett.no>
2022-09-26 20:07:53 +0200

    https://github.com/FritzAndFriends/StreamDeckToolkit template files

---
+++ StreamDeckTradfri/RegisterPluginAndStartStreamDeck.ps1
@@ -0,0 +1,61 @@
+﻿Write-Host "Gathering deployment items..."
+
+Write-Host "Script root: $PSScriptRoot`n"
+
+$basePath = $PSScriptRoot
+
+if ($PSSCriptRoot.Length -eq 0) {
+  $basePath = $PWD.Path;
+}
+
+
+# Load and parse the plugin project file
+$pluginProjectFile = "$basePath\StreamDeckTradfri.csproj"
+$projectContent = Get-Content $pluginProjectFile | Out-String;
+$projectXML = [xml]$projectContent;
+
+$buildConfiguration = "Debug"
+
+# Get the target .net core framework
+$targetFrameworkName = $projectXML.Project.PropertyGroup.TargetFramework;
+
+# Set local path references
+$streamDeckExePath = "$($ENV:ProgramFiles)\Elgato\StreamDeck\StreamDeck.exe"
+
+# For now, this PS script will only be run on Windows.
+$bindir = "$basePath\bin\Debug\$targetFrameworkName\win-x64"
+
+# Make sure we actually have a directory/build to deploy
+If (-not (Test-Path $bindir)) {
+  Write-Error "The output directory `"$bindir`" was not found.`n You must first build the `"StreamDeckTradfri`" project before calling this script.";
+  exit 1;
+}
+
+# Load and parse the plugin's manifest file
+$manifestFile = $bindir +"\manifest.json"
+$manifestContent = Get-Content $manifestFile | Out-String
+$json = ConvertFrom-JSON $manifestcontent
+
+$uuidAction = $json.Actions[0].UUID
+
+$pluginID = $json.UUID
+$destDir = "$($env:APPDATA)\Elgato\StreamDeck\Plugins\$pluginID.sdPlugin"
+
+$pluginName = Split-Path $basePath -leaf
+
+Get-Process -Name ("StreamDeck", $pluginName) -ErrorAction SilentlyContinue | Stop-Process –force -ErrorAction SilentlyContinue
+
+# Delete the target directory, make sure the deployment/copy is clean
+If (Test-Path $destDir) {
+  Remove-Item -Recurse -Force -Path $destDir 
+}
+
+# Then copy all deployment items to the plugin directory
+New-Item -Type Directory -Path $destDir -ErrorAction SilentlyContinue # | Out-Null
+$bindir = $bindir +"\*"
+Copy-Item -Path $bindir -Destination $destDir -Recurse
+
+
+Write-Host "Deployment complete. Restarting the Stream Deck desktop application..."
+Start-Process $streamDeckExePath
+exit 0
---
+++ StreamDeckTradfri/StreamDeckTradfri.cmd
@@ -0,0 +1,2 @@
+﻿cd %appdata%\Elgato\StreamDeck\Plugins\com.yourcompany.plugin.action
+StreamDeckTradfri.exe -break %*
